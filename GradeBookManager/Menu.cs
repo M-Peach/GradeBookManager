aa494dc34b154700e78bd948cbe83b14e060e8d9
M-Peach <mikepeachman@live.com>
2021-12-07 17:57:16 -0500

    Add project files.

---
+++ GradeBookManager/Menu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBookManager
+{
+    internal class Menu
+    {
+        public static void MainMenu()
+        {
+            bool loop = true;
+
+            while (loop)
+            {
+                Console.WriteLine("Grade Manager");
+                Console.WriteLine();
+                Console.WriteLine("[1] Show Classrooms");
+                Console.WriteLine("[2] Add Classerooms");
+                Console.WriteLine("[3] Remove Classroom");
+                Console.WriteLine("[4] Classroom Details");
+                Console.WriteLine("[5] Exit");
+                Console.WriteLine();
+                Console.WriteLine("Select an option: 1 - 5");
+
+
+            ERROR1:
+                string userInput = Console.ReadLine();
+
+                bool isNumber = int.TryParse(userInput, out int userSelection);
+
+                if (isNumber == false)
+                {
+                    Console.WriteLine("ERROR: ENTER A VALID OPTION");
+                    Console.WriteLine();
+                    goto ERROR1;
+                }
+                else if (userSelection == 5)
+                {
+                    loop = false;
+                    return;
+                }
+                else
+                {
+                    Program.CheckSelection(userSelection);
+                }
+
+            ERROR2:
+                Console.WriteLine();
+                Console.WriteLine("Return to menu? (Y / N)");
+
+                userInput = Console.ReadLine().ToUpper();
+
+                if (userInput != "Y")
+                {
+                    goto ERROR2;
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+        }
+
+        public static void DetailsMenu()
+        {
+            Console.Clear();
+            Classrooms.ShowClassrooms();
+            Console.WriteLine();
+            Console.WriteLine("Enter Classroom Number:");
+
+
+
+
+        }
+    }
+}
---
+++ GradeBookManager/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBookManager
+{
+    public class Program
+    {
+        Students students = new Students();
+
+        Classrooms classooms = new Classrooms();
+
+        Menu Menu = new Menu();
+
+        public static void Main()
+        {
+            Menu.MainMenu();
+
+
+        }
+
+        public static void CheckSelection(int userSelection)
+        {
+            switch (userSelection)
+            {
+                case 1:
+                    Classrooms.ShowClassrooms();
+                    break;
+
+                case 2:
+                    Classrooms.AddClassrooms();
+                    break;
+
+                case 3:
+                    Classrooms.RemoveClassrooms();
+                    break;
+
+                case 4:
+                    Menu.DetailsMenu();
+                    break;
+            }
+        }
+    }
+}
